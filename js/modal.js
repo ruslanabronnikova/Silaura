51cb665258dbb4cf061bd7c53927013ea242a7d5
Ruslana <bronnikovars@yandex.ru>
2024-05-07 14:54:07 +0300

    Работа с валидацией и модальным окном

---
+++ js/modal.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const sendButton = document.querySelector('.button'); // Выбираем кнопку "Отправить"
+  const modalSignin = document.getElementById('staticBackdrop');
+
+  sendButton.addEventListener('click', function (event) {
+    event.preventDefault(); // Предотвращаем стандартное действие кнопки "Отправить"
+
+    // Вызываем функцию валидации
+    if (validateEmail()) {
+      // Если валидация успешна, открываем модальное окно
+      var myModal = new bootstrap.Modal(modalSignin);
+      myModal.show();
+    }
+  });
+
+  // Находим крестик для закрытия модального окна
+  const closeButton = modalSignin.querySelector('.btn-close');
+  // Добавляем обработчик события клика на крестик
+  closeButton.addEventListener('click', function () {
+    var myModal = new bootstrap.Modal(modalSignin);
+    myModal.hide(); // Закрываем модальное окно при клике на крестик
+  });
+
+  // Добавляем обработчик события клика на задний фон
+  modalSignin.addEventListener('click', function (event) {
+    if (event.target === modalSignin) {
+      var myModal = new bootstrap.Modal(modalSignin);
+      myModal.hide(); // Закрываем модальное окно при клике на задний фон
+    }
+  });
+});
