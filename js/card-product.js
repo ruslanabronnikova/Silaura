e93d773204c8fdab82f616736715fb52c7615d94
Ruslana <bronnikovars@yandex.ru>
2024-05-14 14:40:11 +0300

    Работа над анимацией

--- js/card-product.js
+++ js/card-product.js
@@ -45,3 +45,24 @@ incrementButtons.forEach((incrementButton, index) => {
         counterValues[index].innerText = (value + 1).toString();
     });
 });
+
+// Получаем все кнопки с классом "btn-save"
+const saveButtons = document.querySelectorAll('.btn-save');
+
+// Для каждой кнопки добавляем обработчик события
+saveButtons.forEach(button => {
+    // Получаем изображение внутри текущей кнопки
+    const saveImage = button.querySelector('.img-save');
+
+    // Добавляем обработчик события клика на кнопку
+    button.addEventListener('click', () => {
+        // Переключаем между изображениями
+        if (saveImage.getAttribute('src') === '/img/Icon/heart-outline.svg') {
+            // Если текущее изображение - пустое сердце, заменяем его на закрашенное
+            saveImage.setAttribute('src', '/img/Icon/heart-fill.svg');
+        } else {
+            // Если текущее изображение - закрашенное сердце, заменяем его на пустое
+            saveImage.setAttribute('src', '/img/Icon/heart-outline.svg');
+        }
+    });
+});
