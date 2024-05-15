12180fbf3bd2992dc8b620cecb146a105b586eac
Ruslana <bronnikovars@yandex.ru>
2024-05-15 16:34:49 +0300

    Готовая верстка, проверка файлов

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const sendButton = document.querySelector('.button'); // Выбираем кнопку "Отправить"
-  const modalSignin = document.getElementById('staticBackdrop');
-
-  sendButton.addEventListener('click', function (event) {
-    event.preventDefault(); // Предотвращаем стандартное действие кнопки "Отправить"
-
-    // Вызываем функцию валидации
-    if (validateEmail()) {
-      // Если валидация успешна, открываем модальное окно
-      var myModal = new bootstrap.Modal(modalSignin);
-      myModal.show();
-    }
-  });
-
-  // Добавляем обработчик события клика на задний фон
-  modalSignin.addEventListener('click', function (event) {
-    if (event.target === modalSignin) {
-      var myModal = new bootstrap.Modal(modalSignin);
-      myModal.hide(); // Закрываем модальное окно при клике на задний фон
-    }
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
   const stars = document.querySelectorAll('.star');
   stars.forEach(star => {
     star.addEventListener('click', function () {
--- js/swiper-init.js
+++ js/swiper-init.js
@@ -1,20 +1,39 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  speed: 800, // Скорость перелистывания в миллисекундах (например, 800 = 0.8 секунды)
-  effect: 'fade', // Эффект "fade" для плавного затухания и появления слайдов
-  fadeEffect: {
-    crossFade: true,
-  },
+var mySwiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 20,
 
   autoplay: {
-    delay: 5000, // Задержка между переключениями в миллисекундах (например, 5000 = 5 секунд)
+    delay: 5000,
+    disableOnInteraction: false,
   },
+  loop: true,
+  breakpoints: {
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    // Когда ширина экрана меньше 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    // Когда ширина экрана меньше 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // Когда ширина экрана меньше 576px
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    }
+  }
 });
+
+
+
